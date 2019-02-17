3d924bb61ad1263288dddfc9538ab9027d6413eb
haoyuliang <haoyuliang199004@163.com>
2019-02-17 16:09:48 +0800

    ssr单用户一键脚本

--- ssr.sh
+++ ssr.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 #=================================================
-#	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
+#	System Required:  CentOS 6+/Debian 6+/Ubuntu 14.04+
 #	Description: Install the ShadowsocksR server
 #	Version: 2.0.38
 #	Author: Toyo
